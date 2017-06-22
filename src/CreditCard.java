775186def527fe3b8a7219eaf71a94af5b1c124a
Joseph Parsons <josephtparsons@gmail.com>
2017-06-22 15:08:09 -0500

    CreditCard comment cleanup

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -3,46 +3,55 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * A Credit Card
+ * A credit card, with both number and expiration date.
+ * (Security code is not currently included.)
  *
- * Created by Joseph T. Parsons on 12/06/17.
- * Modified by Eric Fulwiler
+ * @author  Eric Fulwiler
+ * @version 1.0
+ * @since   2017-06-22
  */
 public class CreditCard implements Serializable {
-   private long cardNumber;
-   private Date expirationDate;
-   private final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
-    
-   /**
-    * @param cardNumber the 16 digit credit card number 
-    * @param expirationDate the credit card expiration date
-    */
-   public CreditCard(long cardNumber, Date expirationDate) {
-       this.cardNumber = cardNumber;
-       this.expirationDate = expirationDate;
-   }
-   
     /**
-     * Gets the credit card number of the user as type long
-     * 
-     * @return cardNumber the 16 digit credit card number of the customer
+     * The 16-digit card number.
+     */
+    private long cardNumber;
+
+    /**
+     * The expiration date of the card.
+     */
+    private Date expirationDate;
+
+    /**
+     * The format to use when displaying the expiration date in {@link CreditCard#toString()}.
+     */
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
+
+
+    /**
+     * @param cardNumber the 16 digit credit card number
+     * @param expirationDate the credit card expiration date
+     */
+    public CreditCard(long cardNumber, Date expirationDate) {
+        this.cardNumber = cardNumber;
+        this.expirationDate = expirationDate;
+    }
+
+
+    /**
+     * @return the 16 digit credit card number, {@link CreditCard#cardNumber}
      */
     public long getCardNumber() {
         return cardNumber;
     }
-    
+
     /**
-     *Gets the expiration date of the credit card as a Date object
-     *
-     * @return expirationDate the expiration date of the credit card
+     * @return the expiration date of the credit card, {@link CreditCard#expirationDate}
      */
     public Date getExpirationDate() {
         return expirationDate;
     }
-    
+
     /**
-     * Overrides the toString method of Object
-     * 
      * @return a string representation of the credit card information
      */
     @Override
