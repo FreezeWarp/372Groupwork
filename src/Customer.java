ee82e1307dd8968d99848f2e986f238bb9d1286f
Joseph Parsons <josephtparsons@gmail.com>
2017-06-29 14:07:14 -0500

    Customer: Insignificant comment cleanup

--- src/Customer.java
+++ src/Customer.java
@@ -96,10 +96,6 @@ public class Customer extends Account {
      *###############################*/
     /**
      * An exception for when trying to remove a {@link CreditCard} from a {@link Customer} when the customer only has one associated {@link CreditCard}.
-     *
-     * @author  Joseph Parsons
-     * @version 1.0
-     * @since   2017-06-22
      */
     class CustomerMinimumCreditCardsException extends Exception {
         CustomerMinimumCreditCardsException() {
