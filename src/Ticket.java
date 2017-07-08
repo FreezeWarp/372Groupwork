4dec157d7d81a4a16af6081c728f8ed21e4cd4ae
Joseph Parsons <josephtparsons@gmail.com>
2017-07-08 15:31:58 -0500

    Ticket comments and cleanup
    Including removing old/unused methods.

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,54 +1,71 @@
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
-import java.io.Serializable;
 
 /**
  * Created by joseph on 26/06/17.
  */
 public class Ticket implements Serializable {
+    /**
+     * The show for the ticket.
+     */
     private Show show;
+
+    /**
+     * The show date of the ticket.
+     */
     private Date date;
+
+    /**
+     * The customer/owner of the ticket.
+     */
     private Customer customer;
 
-    public Ticket(Show show, Customer customer, Date date) {
-        this.show = show;
-        this.customer = customer;
-        this.date = date; 
-    }
-    
     /**
      * The format to use when displaying ticket dates in {@link Ticket#toString()}.
      */
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
+
+    /**
+     * Create a new Ticket using a show, customer, and date.
+     * @param show {@link Ticket#show}
+     * @param customer {@link Ticket#customer}
+     * @param date {@link Ticket#date}
+     */
+    public Ticket(Show show, Customer customer, Date date) {
+        this.show = show;
+        this.customer = customer;
+        this.date = date; 
+    }
+
+
+    /**
+     * @return The ticket's show date, {@link Ticket#date}.
+     */
     public Date getDate() {
         return date;
     }
+
+    /**
+     * @return The ticket's customer/owner, {@link Ticket#customer}.
+     */
     public Customer getCustomer() {
         return customer;
     }
+
+    /**
+     * @return The ticket's show, {@link Ticket#show}.
+     */
     public Show getShow() {
         return show;
     }
 
+    /**
+     * @return The ticket's price, derived from the show price in {@link Ticket#show}.
+     */
     public double getPrice() {
         return show.getTicketPrice();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ticket ticket = (Ticket) o;
-        return Double.compare(ticket.getPrice(), getPrice()) == 0 &&
-                Objects.equals(show, ticket.show) &&
-                Objects.equals(getDate(), ticket.getDate()) &&
-                Objects.equals(getCustomer(), ticket.getCustomer());
-    }
-
-    public void adjustClientBalance() {
-        System.out.println("Client balance is now $"+ show.getClient().getBalance()); // TODO: refactor this line into the invoking method.
     }
     
     /**
